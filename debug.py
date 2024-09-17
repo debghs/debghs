ef93f91608f7b7908b89c8d90c62e622ee501b51
debghs <dghosh31428@gmail.com>
2024-09-18 02:24:16 +0530

    fix

--- debug.py
+++ debug.py
@@ -44,16 +44,9 @@ def fetch_repositories(username):
             repositories(first: 100, after: $cursor) {
                 nodes {
                     name
-                    refs(refPrefix: "refs/heads/", first: 100) {
+                    refs(refPrefix: "refs/heads/", first: 10) {  # Limit number of refs to avoid excessive nodes
                         nodes {
                             name
-                            target {
-                                ... on Commit {
-                                    history(first: 100) {
-                                        totalCount
-                                    }
-                                }
-                            }
                         }
                     }
                 }
@@ -86,13 +79,52 @@ def count_user_contributions(username):
     total_commits = 0
 
     for repo in repos:
-        for ref in repo['refs']['nodes']:
-            commits = ref['target']['history']['totalCount']
-            if commits > 0:
-                contributed_to += 1
-            total_commits += commits
+        commits_count = count_commits_in_repo(username, repo['name'])
+        if commits_count > 0:
+            contributed_to += 1
+        total_commits += commits_count
 
     return len(repos), contributed_to, total_commits
+
+def count_commits_in_repo(username, repo_name):
+    query = '''
+    query($login: String!, $repoName: String!, $cursor: String) {
+        repository(owner: $login, name: $repoName) {
+            refs(refPrefix: "refs/heads/", first: 10, after: $cursor) {
+                nodes {
+                    name
+                    target {
+                        ... on Commit {
+                            history(first: 100) {  # Get first 100 commits for each ref
+                                totalCount
+                            }
+                        }
+                    }
+                }
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+            }
+        }
+    }'''
+    variables = {'login': username, 'repoName': repo_name, 'cursor': None}
+    total_commits = 0
+    while True:
+        response = simple_request(query, variables)
+        response_data = response.json()
+        print("API Response:", response_data)  # Debugging line
+        data = response_data.get('data', {})
+        repo = data.get('repository', {})
+        refs = repo.get('refs', {})
+        for ref in refs.get('nodes', []):
+            total_commits += ref['target']['history']['totalCount']
+        page_info = refs.get('pageInfo', {})
+        if page_info.get('hasNextPage'):
+            variables['cursor'] = page_info.get('endCursor')
+        else:
+            break
+    return total_commits
 
 def get_user_commits(username):
     query = '''
@@ -123,7 +155,7 @@ if __name__ == '__main__':
         total_commits = get_user_commits(USER_NAME)
         
         with open('README.md', 'w') as f:
-            f.write(f"# User Information\n\n")
+            f.write(f"[![An image of @debghs's Holopin badges, which is a link to view their full Holopin profile](https://holopin.me/debghs)](https://holopin.io/@debghs)\n\n")
             f.write(f"- Account Created: {acc_date}\n")
             f.write(f"- Age: {age_data}\n")
             f.write(f"- Number of Repositories: {num_repos}\n")
